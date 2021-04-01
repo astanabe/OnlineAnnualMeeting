da1bfee9ba8b8d63a6b7dbc75a80a64ed0c71c7e
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2021-04-02 08:13:46 +0900

    Update contents

--- onlineannualmeeting.sh
+++ onlineannualmeeting.sh
@@ -7,20 +7,20 @@ mv onlineannualmeeting.temp.bbl onlineannualmeeting.bbl
 uplatex --kanji=utf8 onlineannualmeeting
 uplatex --kanji=utf8 onlineannualmeeting
 dvipdfmx onlineannualmeeting
-rm onlineannualmeeting.temp.pdf
+rm -f onlineannualmeeting.temp.pdf
 perl -i.bak -npe "s/<dc:date>.+<\/dc:date>/<dc:date>$t<\/dc:date>/" onlineannualmeeting.xmp
 cpdf -set-metadata onlineannualmeeting.xmp onlineannualmeeting.pdf -o onlineannualmeeting.temp.pdf
-rm onlineannualmeeting.pdf
+rm -f onlineannualmeeting.pdf
 mv onlineannualmeeting.temp.pdf onlineannualmeeting.pdf
 perl convert4latexml.pl < onlineannualmeeting.tex > onlineannualmeeting.temp.tex
 latexml --xml --nocomments --inputencoding=utf8 --destination=onlineannualmeeting.xml onlineannualmeeting.temp.tex
-rm onlineannualmeeting.temp.tex
+rm -f onlineannualmeeting.temp.tex
 perl convertxml2xml.pl < onlineannualmeeting.xml > onlineannualmeeting.temp.xml
-rm onlineannualmeeting.xml
+rm -f onlineannualmeeting.xml
 mv onlineannualmeeting.temp.xml onlineannualmeeting.xml
 latexmlpost --format=html5 --crossref --index --mathimages --nomathsvg --nopresentationmathml --nocontentmathml --noopenmath --nomathtex --graphicimages --verbose --destination=onlineannualmeeting.html onlineannualmeeting.xml
 perl converthtml2html.ja.pl < onlineannualmeeting.html > onlineannualmeeting.temp.html
-rm onlineannualmeeting.html
+rm -f onlineannualmeeting.html
 mv onlineannualmeeting.temp.html onlineannualmeeting.html
 perl -i.bak -npe "s/<dc:date>.+<\/dc:date>/<dc:date>$t<\/dc:date>/" onlineannualmeeting.opf
 ebook-convert onlineannualmeeting.html onlineannualmeeting.epub --max-toc-links=0 --toc-threshold=1 --level1-toc=//h:h2 --level2-toc=//h:h3 --level3-toc=//h:h4 --read-metadata-from-opf=onlineannualmeeting.opf --preserve-cover-aspect-ratio
